0cfc5937c6c8bc8ccfc0b7bf2cc535c5a5d3e8ef
Emanuel Farcy <emanuelfarcy@gmail.com>
2022-05-10 17:11:27 -0300

    Desafío 02 ingreso de array en un objeto. página accesories. Carga de productos.

---
+++ js/main2.js
@@ -0,0 +1,31 @@
+//1-Creo un array vacio llamado productos
+
+const productos = [];
+
+//2. Creo una Función Productos 
+function Productos(accesorio, precio, stock){
+	this.accesorio= accesorio;
+	this.precio= precio;
+	this.stock= stock
+}
+
+//3. Creo una función para crear accesorios
+const crearProductos= (accesorio, precio, stock) => {
+	const producto= new Productos(accesorio, precio, stock);
+	return producto;
+}
+
+//4. Ahora debo crear una función para guardar el producto ingresado por el usuario
+const agregarAProductos = (objetoProducto)=>{
+	productos.push(objetoProducto);
+};
+
+//5. Pedir los datos al usuario
+let accesorio= prompt("Ingrese un accesorio de ciclismo: ");
+let precio= prompt("Ingrese un precio: ");
+let stock= prompt("Ingrese la cantidad del producto: ");
+
+const instanciaDeProducto= crearProductos(accesorio, precio, stock);
+agregarAProductos(instanciaDeProducto);
+console.log(instanciaDeProducto);
+console.log(productos);
